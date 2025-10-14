b91babc8cab412e42ab57a2e0bda4b570da764f2
Christian Leuschner <el_maestro@tutanota.com>
2025-10-14 22:42:52 +0200

    use up to date module list

--- src/app/start/hooks/useModuleList.ts
+++ src/app/start/hooks/useModuleList.ts
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import moduleData from "../../../data/master.json";
+import moduleData from "../../../data/master_25.json";
 import { Module } from "@/types/module";
 import { ModuleType, Turnus } from "@/utils/enums";
 
